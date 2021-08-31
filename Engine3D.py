75a0e002514c1f1a1a57a9e385260cbb6d3aaa3a
Mirka <mirkanicolle@gmail.com>
2021-08-30 23:11:10 -0600

    done

--- Engine3D.py
+++ Engine3D.py
@@ -3,3 +3,33 @@
 
 #Mirka Monzon 18139
 #Lab2: Shaders
+
+from gl import *
+from textura import Texture
+
+bmp = Bitmap(1000, 1000)
+
+def glInit():
+    return bmp
+
+
+if __name__ == '__main__':
+    '''Main Program'''
+
+    #Inicializa obj
+    bmp = glInit()
+
+    bmp.glCreateWindow(1000, 1000)
+
+    #Cambia todos los pixeles de un color
+    bmp.glClear()
+
+    #Set pixel Colors
+    bmp.glColor(1, 1, 1)
+
+    bmp.lookAt(V3(-0.2,0,5),V3(0,0,0),norm(V3(0,1,0)))
+    bmp.loadViewportMatrix(0, 0)
+    bmp.glLoadObjModel("model.obj", translate=(-1.0,-0.9,0), scale=(0.04,0.04,0.04), rotate=(0,0.2,0))
+    
+    #Output BMP
+    bmp.glWrite("phong.bmp")
